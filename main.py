695f436f6bd118ead96e92bec3ea3632f2d36f9d
Michel Hijazin <michaelhijazeen203@gmail.com>
2023-05-03 16:10:31 -0400

    Wrote basic files

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.even.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit
+
+            self.screen.full('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()        
